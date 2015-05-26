01d18bd589ab7241984ca40d540314bed39e3489
nanomuthu <bimuthukumar@yahoo.com>
2015-05-26 17:03:55 +0530

    Update cachematrix.R
    Please include comments and check for the change in argument to makeCacheMatrix inorder to solve using cachesolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## Write a short comment describing this function
 
 
-makeCacheMatrix <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
     x <<- y
